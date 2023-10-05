941aba733dbc6b1ccd21d9a06b22cb042c410266
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-10-05 02:15:41 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("\tPrograma para hallar la sumatoria de una secuencia");
+Console.WriteLine("\t**************************************************\n");
+Console.Write("Ingrese el valor de n: ");
+int n = int.Parse(Console.ReadLine()!);
+float sumatoria, resultado;
+sumatoria = 0;
+
+for (float i = 1; i <= n; i++)
+{
+    resultado = (16-i)/(3*i);
+    Console.WriteLine($"Resultado {i}: {resultado}");
+    sumatoria += resultado;
+}
+
+Console.WriteLine("Sumatoria: " + sumatoria);
+
